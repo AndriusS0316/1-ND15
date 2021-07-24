11f26b890e41bff729f0ea75089003a363d4b526
Andrius <andriuss0316@gmail.com>
2021-07-24 21:44:59 +0300

    20210724

--- main.js
+++ main.js
@@ -2,4 +2,8 @@
 
 function generuotiSocials(selector, iconsList) {
     // your code goes here
-  }
+}
+
+const icons = ['facebook', 'instagram', 'twitter', 'linkedin'];
+
+generuotiSocials('.socials', icons);
